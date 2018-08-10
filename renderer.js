22a0c204dbe94dd5d17cbe1f6fe010d0a0654f0d
lzy46834 <lzy46834@ly.com>
2018-08-10 11:32:43 +0800

    temp work

--- renderer.js
+++ renderer.js
@@ -1,9 +1,62 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+var http = require('http');
+const format = require('date-fns/format');
 
-console.log('hello world!')
+function doNotify(evt) {
+  http.get("http://free_bus_ticket.fyxmt.com/interface/ticketList?queryDate=" + format(new Date(), "YYYY-MM-DD"), res => {
+    let r = '';
+    res.on('data', chunk => {
+      r += chunk;
+    });
+    res.on('end', () => {
+      result = JSON.parse(r);
+      if (result && result['success']) {
+        let isNight = new Date().getHours() >= 19 || new Date().getHours() < 8;
+        if (isNight) {
+          id = result['obj']['busTrips'][0]['trips'][1]['id']
+        }
+        else {
+          id = result['obj']['busTrips'][2]['trips'][0]['id']
+        }
+        sendData = { 'wechatNo': 'ofqo-uJLT6jux0jk8vm4vlPLIDCE', 'id': id }
+      }
+    })
+  });
 
-document.body.addEventListener('click', () => {
-  console.log('hello vscode!')
+
+
+
+
+
+
+
+
+
+
+
+  sendData = getSendData("http://free_bus_ticket.fyxmt.com/interface/ticketList?queryDate=" + format(new Date(), "YYYY-MM-DD"), true);
+
+  var options = {
+    hostname: 'free_bus_ticket.fyxmt.com',
+    path: '/interface/grabTicket',
+    method: 'POST'
+  };
+  var req = http.request(options, res => {
+    let r = '';
+    let result;
+    res.on('data', chunk => {
+      r += chunk;
+    });
+    res.on('end', () => {
+      result = JSON.parse(r);
+    })
+  });
+  req.write(JSON.stringify(sendData));
+  req.end();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById("basic").addEventListener("click", doNotify1);
 })
